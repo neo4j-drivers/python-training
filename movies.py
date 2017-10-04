4a668e54a45aa94f47ce3376cc22cb009c39436c
Adam Cowley <adam@adamcowley.co.uk>
2017-10-04 17:23:17 +0100

    Save ratings within a transaction

--- movies.py
+++ movies.py
@@ -30,6 +30,36 @@ def match_random_movie(tx, genre, ignore):
     record = tx.run(cypher, genre=genre, ignore=ignore).single()
     return dict(record[0]), dict(record[1]) if record else None
 
+def save_ratings(tx, user_id, genre, ratings):
+    """ Merge the User node by User ID
+    """
+    tx.run("MERGE (u:User {userId: $user_id})", user_id=user_id)
+
+    """ Save each rating
+    """
+    for movieId, rating in ratings.items():
+        tx.run("MATCH (u:User {userId: $user_id}) "
+               "MATCH (m:Movie {imdbId: $movie_id}) "
+               "MERGE (u)-[r:RATED]->(m) "
+               "SET r.rating = $rating ", user_id=user_id, movie_id=movieId, rating=rating)
+
+def get_recommendation(tx, user_id, genre):
+    """ Get a recommendation
+    """
+    record = tx.run("MATCH (g:Genre {name: $genre}) "
+                    "MATCH (u:User {userId: $user_id})-[x:RATED]->()<-[y:RATED]-(other) "
+                    "WITH g, u, other, COUNT(*) AS numbermovies, SUM(x.rating * y.rating) AS xyDotProduct, "
+                    "SQRT(REDUCE(xDot = 0.0, a IN COLLECT(x.rating) | xDot + a^2)) AS xLength, "
+                    "SQRT(REDUCE(yDot = 0.0, b IN COLLECT(y.rating) | yDot + b^2)) AS yLength "
+                    "WITH g, u, other, xyDotProduct / (xLength * yLength) AS sim "
+                    "ORDER BY sim DESC LIMIT 10 "
+                    "MATCH (other)-[r:RATED]->(recommendation)-[:IN_GENRE]->(g) "
+                    "WHERE r.rating >= 4 AND NOT (u)-[:RATED]->(recommendation) "
+                    "RETURN g, recommendation, count(*) as occurrences "
+                    "ORDER BY occurrences DESC LIMIT 1", user_id=user_id, genre=genre).single()
+
+    return dict(record[0]), dict(record[1]) if record else None
+
 @app.route("/")
 def get_index():
     """ Show the index page.
@@ -44,3 +74,24 @@ def start_recommendation(genre):
     with driver.session() as session:
         genre, movie = session.read_transaction(match_random_movie, genre, [])
         return render_template("rate.html", genre=genre, movie=movie)
+
+@app.route("/recommend/<genre>/next")
+def get_next_movie(genre):
+    with driver.session() as session:
+        ignore = request.args.get("rated", "").split(",")
+        genre, movie = session.read_transaction(match_random_movie, genre, ignore)
+        return jsonify(movie)
+
+@app.route("/recommend/<genre>/results")
+def get_results(genre):
+    with driver.session() as session:
+        user_id = str(uuid.uuid4())
+        param = request.args.get("ratings", "")
+        ratings = json.loads(param)
+
+        session.write_transaction(save_ratings, user_id, genre, ratings)
+
+        genre, movie = session.read_transaction(get_recommendation, user_id, genre)
+
+        return render_template("result.html", genre=genre, movie=movie)
+
---
+++ static/rate.js
@@ -0,0 +1,49 @@
+/**
+ * This is some ugly looking jQuery that gets the job done
+ */
+!function($) {
+    var ratings = {};
+
+    function getNextMovie() {
+        $.get(document.location.pathname+'/next?rated='+ Object.keys(ratings).join(','), function(res) {
+            console.log(res);
+            $('.film').attr('id', 'movie_'+res.imdbId);
+            $('.title').html(res.title);
+            $('.poster').attr('src', res.poster);
+            $('.plot').html(res.plot);
+            $('.runtime').html(res.runtime);
+            $('.year').html(res.year);
+            $('.imdbRating').html(res.imdbRating);
+        });
+    }
+
+    function saveRating(id, rating) {
+        ratings[id] = rating;
+
+        // If 10 films have been rated, take the user to the recommendation page
+        if ( Object.keys(ratings).length == 10 ) {
+            document.location.href = document.location.pathname +'/results?ratings='+ JSON.stringify(ratings);
+
+            return;
+        }
+
+        // Else load the next movie
+        getNextMovie();
+    }
+
+    $(".downvote").click(function(e) {
+        e.preventDefault();
+
+        var movie_id = $('.film').attr('id').replace('movie_', '');
+
+        saveRating(movie_id, 2);
+    });
+
+    $(".upvote").click(function(e) {
+        e.preventDefault();
+
+        var movie_id = $('.film').attr('id').replace('movie_', '');
+
+        saveRating(movie_id, 2);
+    });
+}(jQuery);
