381f706a66599955875f51898ffb6e815a7cbaa9
Adam Cowley <adam@adamcowley.co.uk>
2017-10-04 17:20:38 +0100

    Get a random movie in the selected genre

--- movies.py
+++ movies.py
@@ -18,9 +18,29 @@ def match_genres(tx):
     return tx.run("MATCH (genre:Genre) "
                   "RETURN genre.name AS name "
                   "ORDER BY genre.name").data()
+
+def match_random_movie(tx, genre, ignore):
+    cypher = "MATCH (g:Genre)<-[:IN_GENRE]-(m:Movie) WHERE g.name = $genre "
+
+    if ignore:
+        cypher += " AND NOT m.imdbId IN $ignore "
+
+    cypher += "RETURN g, m  ORDER BY RAND() LIMIT 1"
+
+    record = tx.run(cypher, genre=genre, ignore=ignore).single()
+    return dict(record[0]), dict(record[1]) if record else None
+
 @app.route("/")
 def get_index():
     """ Show the index page.
     """
     with driver.session() as session:
         return render_template("index.html", genres=session.read_transaction(match_genres))
+
+@app.route("/recommend/<genre>")
+def start_recommendation(genre):
+    """ Get a movie within this genre at random
+    """
+    with driver.session() as session:
+        genre, movie = session.read_transaction(match_random_movie, genre, [])
+        return render_template("rate.html", genre=genre, movie=movie)
